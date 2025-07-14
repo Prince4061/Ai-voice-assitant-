e6b4c2bcb410ceaa4b1e1082ab05488582bf3c13
Prince4061 <89417468+Prince4061@users.noreply.github.com>
2025-07-14 23:03:08 +0530

    Update open.py

--- open.py
+++ open.py
@@ -1,5 +1,7 @@
 import os
 import pyttsx3
+import webbrowser
+
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -8,6 +10,21 @@ engine.setProperty('voices',voices[1].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("mai sun raha hu ......")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        query = r.recognize_google(audio,language='en-in')
+        print(f"User said: {query}")
+    except Exception as e:
+        print(e)
+        print("sorry mai sun nahi paya, phir se bolo")
+        return "None"
+    return query 
 
 
 def OpenExe(query):
@@ -21,8 +38,9 @@ def OpenExe(query):
     elif "ppt" in query or "powerpoint" in query:
         path = "C:\\Program Files\\Microsoft Office\\root\\Office16\\POWERPNT.EXE"
         os.startfile(path)
-        
-
+    
+    elif "open youtube" in query:
+        webbrowser.open("www.youtube.com")
         
     else:
         speak("Sorry,dont know how to open it.")
